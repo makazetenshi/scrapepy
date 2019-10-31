c8332417a4e3a55b20a99c7a6fac3226dc835a0b
makazetenshi <frejsps2@gmail.com>
2019-11-01 00:04:02 +0100

    Adds a help menu

--- Scraper.py
+++ Scraper.py
@@ -41,25 +41,45 @@ class Scraper(object):
         else:
             print(Parser.find_all_link_urls(content))
 
+    @staticmethod
+    def display_help_menu():
+        help_string = 'usage: Scrape.py <mode> <target> [option] ... [tag] [w]'
+        help_string = help_string + '\n Modes: '
+        help_string = help_string + '-t : Find all with tag, requires the tag arg\n'
+        help_string = help_string + '-i : Find all image urls\n'
+        help_string = help_string + '-a : Find all link urls\n'
+        help_string = help_string + '-h : Display help menu\n'
+        help_string = help_string + '\n'
+        help_string = help_string + 'Optional:\n'
+        help_string = help_string + '[tag] : Needs to be specified for finding all of a certain tag type\n'
+        help_string = help_string + '[w] : If this parameter is used, ' \
+                                    'the result of the search will be outputted to a .txt file in file dir'
+        print(help_string)
+
     def main(self):
         args = sys.argv
         if len(args) < 2:
             print('Please add arguments to proceed.')
         else:
-            target = args[1]
-            mode = args[2]
+            if len(args) > 2:
+                target = args[2]
+            else:
+                target = ''
+            mode = args[1]
             mode = mode.replace('-', '')
-            if mode != 'help' and mode != 'h':
-                if len(args) >= 4 and mode == 't':
-                    tags = args[3]
-                else:
-                    tags = ''
+            if len(args) >= 4 and mode == 't':
+                tags = args[3]
+            else:
+                tags = ''
 
-                if 'w' in args:
-                    write = True
-                else:
-                    write = False
+            if 'w' in args:
+                write = True
+            else:
+                write = False
 
+            if mode == 'h' or mode == 'help':
+                self.display_help_menu()
+            else:
                 fetcher = Fetcher
                 page = fetcher.get_page(target)
                 content = page.content
