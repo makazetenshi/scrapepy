b250da8c9ecbaab08f4b1105d74d78b2005eb7ba
makazetenshi <frejsps2@gmail.com>
2021-10-31 23:34:21 +0100

    Adds Recursive disabling and fixes some documentation

--- Parser.py
+++ Parser.py
@@ -3,30 +3,30 @@ from bs4 import BeautifulSoup
 
 class Parser:
     @staticmethod
-    def find_elements_by_tag(content, tag, limit):
+    def find_elements_by_tag(content, tag, limit, recursive):
         if tag == '':
             return 'No tag specified.'
         soup = BeautifulSoup(content, 'html.parser')
-        return soup.find_all(tag, limit=limit)
+        return soup.find_all(tag, limit=limit, recursive=recursive)
 
     @staticmethod
-    def find_all_image_urls(content, limit):
+    def find_all_image_urls(content, limit, recursive):
         soup = BeautifulSoup(content, 'html.parser')
         urls = []
-        for image in soup.find_all('img', limit=limit):
+        for image in soup.find_all('img', limit=limit, recursive=recursive):
             urls.append(image.get('src'))
         return urls
 
     @staticmethod
-    def find_all_link_urls(content, limit):
+    def find_all_link_urls(content, limit, recursive):
         soup = BeautifulSoup(content, 'html.parser')
         urls = []
-        for link in soup.find_all('a', limit=limit):
+        for link in soup.find_all('a', limit=limit, recursive=recursive):
             urls.append(link.get('src'))
         return urls
 
     @staticmethod
-    def find_all_with_class(content, class_name, limit):
+    def find_all_with_class(content, class_name, limit, recursive):
         soup = BeautifulSoup(content, 'html.parser')
-        result = soup.find_all(attrs={"class": class_name}, limit=limit)
+        result = soup.find_all(attrs={"class": class_name}, limit=limit, recursive=recursive)
         return result
